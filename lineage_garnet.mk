b6d4423bed8b0520df0b0517b89e45eab6f63eb7
bezke <bezke1@gmail.com>
2025-02-06 07:32:16 +0100

    Revert "garnet: Welcome to `RisingOS`"
    This reverts commit 68ee10408d947b2d612b65458cf9a066ff54fd80.

--- lineage_garnet.mk
+++ lineage_garnet.mk
@@ -12,13 +12,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# RisingOS
-WITH_GMS := true
-TARGET_ENABLE_BLUR := false
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := false
-TARGET_DEFAULT_PIXEL_LAUNCHER := true
-
 # Inherit from garnet device
 $(call inherit-product, device/xiaomi/garnet/device.mk)
 
@@ -32,8 +25,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="garnet_global-user 14 UKQ1.231003.002 V816.0.17.0.UNRMIXM release-keys" \
     BuildFingerprint=Redmi/garnet_global/garnet:14/UKQ1.231003.002/V816.0.17.0.UNRMIXM:user/release-keys \
     DeviceName=garnet \
-    DeviceProduct=garnet_global \
-    RisingChipset="Snapdragon® 7s Gen 2" \
-    RisingMaintainer="Mohammad Kibria"
+    DeviceProduct=garnet_global
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
